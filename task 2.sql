81c49f04f675f8512dfa4e25e73ae16d2e4d2904
Aniket Khandade <83821549+aniketkhandade9@users.noreply.github.com>
2025-06-25 20:04:01 +0530

    Add files via upload

---
+++ task 2.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE CompanyDB;
+USE CompanyDB;
+CREATE TABLE Employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    department VARCHAR(100),
+    email VARCHAR(100) DEFAULT 'not_provided@example.com'
+);
+
+INSERT INTO Employees (id, name, department, email)
+VALUES (1, 'Alice Johnson', 'HR', 'alice@example.com');
+
+INSERT INTO Employees (id, name, department, email)
+VALUES (2, 'Bob Smith', NULL, DEFAULT);
+
+INSERT INTO Employees (id, name, department)
+VALUES (3, 'Charlie Brown', 'IT');
+
+UPDATE Employees
+SET department = 'Finance'
+WHERE id = 2;
+
+SELECT * FROM Employees;
